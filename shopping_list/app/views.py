76b3fd1267dfa0e66b19ee383996c64c699ff391
Chris Wachira <chriskane816@gmail.com>
2017-09-30 17:24:37 +0300

    Add Login view

--- shopping_list/app/views.py
+++ shopping_list/app/views.py
@@ -1,9 +1,9 @@
 from flask import redirect, flash, make_response, render_template, url_for, session
-from forms import RegistrationForm
+from forms import RegistrationForm, LoginForm
 from flask.views import View
-from models import User, add_user
+from models import User, add_user, get_user
 
-class Index(View):
+class Register(View):
     methods = ['GET', 'POST']
 
     def dispatch_request(self):
@@ -33,3 +33,27 @@ class Index(View):
         return render_template('signup.html', form=reg_form)
 
 
+class Login(View):
+    methods = ['GET', 'POST']
+
+    def dispatch_request(self):
+        login_form = LoginForm()
+
+        if login_form.validate_on_submit():
+            search = get_user(login_form.username.data)
+
+            if search != None:
+                print("Search result: ", search)
+                if login_form.password.data == search['pwd']:
+                    flash('Logged in successfully!')
+                    login_form.username.data = ""
+                    login_form.password.data = ""
+                else:
+                    flash('Username or password incorrect!')
+                    login_form.username.data = ""
+                    login_form.password.data = ""
+            else:
+                flash('Username not found! Check again...')
+
+
+        return render_template('login.html', form=login_form)
