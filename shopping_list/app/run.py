7e61d8ddbf81a68b3c87626358609e11c50f1dda
Chris Wachira <chriskane816@gmail.com>
2017-09-30 17:25:33 +0300

    Add add_url_rule() for Login page

--- shopping_list/app/run.py
+++ shopping_list/app/run.py
@@ -2,14 +2,15 @@
 from flask import Flask
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from views import Index
+from views import Register, Login
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "iAMROOT"
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
-app.add_url_rule('/', view_func=Index.as_view('index'))
+app.add_url_rule('/register', view_func=Register.as_view('register'))
+app.add_url_rule('/login', view_func=Login.as_view('login'))
 
 if __name__ == "__main__":
     manager.run()
